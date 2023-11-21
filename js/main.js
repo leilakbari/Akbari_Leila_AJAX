3eb3abc2e0b4981dbc79cd708bda3c90ad113009
leilakbari <l_akbari@fanshaweonline.ca>
2023-11-21 13:50:46 -0500

    spinner edit

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
   // const infoboxesCon = document.querySelector("#infoBoxes");
   const materialsCon = document.querySelector("#materials-con");
   const materialsTemplate = document.querySelector("#material-template");
+  // const spinner = document.getElementById('spinner');
   
 
   //This information needs to be removed then pulled with an AJAX Call using the Fetch API
@@ -103,6 +104,9 @@
   // <path fill="#333" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
   // <animateTransform attributeName="transform" attributeType="XML" type="rotate" dur="1s" from="0 50 50" to="360 50 50" repeatCount="indefinite" /> </path> </svg>`;
 
+  // Spinner Function
+
+
  
 //functions
 function modelLoaded() {
@@ -127,7 +131,7 @@ function modelLoaded() {
   
           const imgElement = document.createElement('img');
           // imgElement.src = infoBox.picture.thumbnail;
-          
+
           // imgElement.src = `images/${index.thumbnail}`;
 
           const imagePath = `images/${infoBox.thumbnail}`;
